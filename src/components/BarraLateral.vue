acac3920991500c98249479de6b3f5ad981979c6
Wesley Dutra Paiz <wesleydtrp19@gmail.com>
2023-02-12 21:21:30 -0300

    Refatoração do doc Formulário.vue

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+        <div class="text-center">
+            <img src="../assets/logo.png" alt="logo teste do tracker" id="logo">
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent ({
+        name: 'BarraLateral'
+    })
+</script>
+
+<style scoped>
+    header {
+        background: #019367e3;
+        width: 100%;
+        height: 100vh;
+        padding: 10px;
+    }
+
+    #logo {
+        width: 150px;
+    }
+
+    @media only screen and (max-width: 768px) {
+        header {
+            padding: 15px;
+            height: auto;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        #logo {
+            width: 150px;
+            margin: auto;
+        }
+    }
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column id-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa deseja iniciar?">
+            </div>
+            <div class="column">
+                <Temporizador />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import Temporizador from "./Temporizador.vue";
+
+    export default defineComponent ({
+        name: 'Formulario',
+        components: {
+            Temporizador
+        },
+        
+    })
+</script>
+
+<style>
+    .box {
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.377);
+    }
+</style>
